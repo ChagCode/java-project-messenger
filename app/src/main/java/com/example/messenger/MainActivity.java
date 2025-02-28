d32ba8d1558cb3a0f8bacc7c1d48e88cbc6d098e
chagcode <chag.open@gmail.com>
2025-02-28 17:50:51 +0300

    second commit

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.messenger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button buttonSendMessage = findViewById(R.id.buttonSendMessage);
+        EditText editTextMessage = findViewById(R.id.editTextMessage);
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = editTextMessage.getText().toString();
+                launchNextScreen(message);
+
+            }
+        });
+        }
+    // метод, который запускает следующий экран и запускается при клике на кнопку
+    private void launchNextScreen(String message) {
+        // экземпляр Intent служит для запуска нового окна
+        // this - это класс, который хочет запустить новое окно (в нашем случае это - MainActivity)
+        // MessageActivity - это класс окна, который мы хотим запустить
+        Intent intent = new Intent(this, MessageActivity.class);
+        // В Intent есть метод, который может направлять данные на новый экран
+        // ключ: "message", значение: message
+        intent.putExtra("message", message);
+        // запускаем новую активити
+        startActivity(intent);
     }
 }
